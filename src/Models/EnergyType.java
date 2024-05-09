0aa4c8588499b7b23086ad9535fcdd2da0a0bf04
Vector158 <viktorskokins@gmail.com>
2024-05-09 02:06:29 +0200

    Seminar 2.1

---
+++ src/Models/EnergyType.java
@@ -0,0 +1,2 @@
+package Models;
+public enum EnergyType {Petrol, Diesel, Gas, Hybrid, Electric, Other, not_specified}
---
+++ src/Services/MainService.java
@@ -0,0 +1,7 @@
+package Services;
+
+public class MainService {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
